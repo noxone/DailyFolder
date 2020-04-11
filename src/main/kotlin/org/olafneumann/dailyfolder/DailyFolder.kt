e152a7f20de83e4db458219787654894942d5e76
Olaf Neumann <git-account@olafneumann.org>
2020-04-11 11:52:41 +0200

    add documentation plugin

--- src/main/kotlin/org/olafneumann/dailyfolder/DailyFolder.kt
+++ src/main/kotlin/org/olafneumann/dailyfolder/DailyFolder.kt
@@ -6,7 +6,12 @@ import org.olafneumann.dailyfolder.helper.FolderHelper.desktopAvailable
 import org.olafneumann.dailyfolder.view.FolderDeletionView
 import tornadofx.*
 
+/** main application class defining the initial view to be shown */
 class DailyFolderApp : App(FolderDeletionView::class) {
+    /**
+     * Executed when the application is about to be started by JavaFX launcher.<p>
+     * This implementation checks whether the desktop folder is known. If not an error message will be shown and the application start will be aborted.
+     */
     override fun start(stage: Stage) {
         if (!desktopAvailable) {
             alert(
@@ -22,4 +27,5 @@ class DailyFolderApp : App(FolderDeletionView::class) {
     }
 }
 
+/** Main application entry point */
 fun main() = launch<DailyFolderApp>()
