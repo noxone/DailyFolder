8eab30783bffb03fff145b003f6ce7f86cb7066b
Olaf Neumann <git-account@olafneumann.org>
2020-04-12 09:20:44 +0200

    improve folder implementation

--- src/main/kotlin/org/olafneumann/dailyfolder/model/Folder.kt
+++ src/main/kotlin/org/olafneumann/dailyfolder/model/Folder.kt
@@ -6,6 +6,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.stream.Collectors
 
 class Folder(
     private val path: Path
@@ -13,29 +14,50 @@ class Folder(
     constructor() : this(FolderHelper.getDesktopFolder() ?: FileSystems.getDefault().rootDirectories.first()!!)
 
     companion object {
+        /** default object using the platforms desktop folder */
         val DEFAULT = Folder()
+
+        /** Formatter that produces the desired folder name */
         private val dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE
-        private val folderPattern = Regex("^\\d{4}-\\d{2}-\\d{2}$")
 
-        private fun Path.listPaths() =
-            Files.newDirectoryStream(this).use { stream -> stream.mapNotNull { it }.toList() }
+        /** regex describing the desired folder name format */
+        private val folderPattern = Regex("^(?<year>[0-9]{4})-(?<month>[0-9]{1,2})-(?<day>[0-9]{1,2})$")
 
-        private fun Path.matchesFolderPattern() = fileName.toString().matches(folderPattern)
-        private fun Path.isEmpty() = listPaths().isEmpty()
-        private fun Path.listNames() = listPaths().map { it.fileName!!.toString() }
-
+        /** This value returns the today's current folder name */
         private val currentDailyFolderName: String get() = dateFormatter.format(LocalDate.now())
+
+        /** Predicate testing whether the current path matches the daily folder name pattern */
+        private fun Path.matchesFolderPattern() = fileName.toString().matches(folderPattern)
+
+        /** List all contents of a given path if it is a directory. Returns an empty list if the directory is empty
+         * or the path is not a directory*/
+        private fun Path.listPaths(): List<Path> =
+            if (Files.isDirectory(this))
+                Files.list(this)
+                    .use { stream ->
+                        stream.filter { it != null }
+                            .map { it!! }
+                            .collect(Collectors.toList())
+                    }
+            else
+                emptyList()
+
+        /**Checks whether the current Path is an empty directory (or not a directory at all)*/
+        private fun Path.isEmpty() =
+            if (Files.isDirectory(this))
+                Files.list(this).use { it.limit(2).count() == 0L }
+            else
+                false
     }
 
     val dailyFolders: List<Path> get() = path.listPaths().filter { it.matchesFolderPattern() }
     val currentDailyFolder: Path get() = path.resolve(currentDailyFolderName)
 
-    fun listContentNames(path: Path) = path.listNames()
-    //fun getSubfolderContents() = dailyFolders.map { it to it.listNames() }
+    fun listContentNames(path: Path) = path.listPaths().map { it.fileName!!.toString() }
 
     fun createFolderForToday() = Files.createDirectories(currentDailyFolder)!!
 
-    private fun deleteFolders(predicate: (Path) -> Boolean)= dailyFolders.filter(predicate).forEach(Files::delete)
+    fun deleteFolders(predicate: (Path) -> Boolean) = dailyFolders.filter(predicate).forEach(Files::delete)
     fun deleteEmptyFolders() = deleteFolders { it.isEmpty() && it.name != currentDailyFolderName }
 
     private val Path.name: String get() = fileName.toString()
